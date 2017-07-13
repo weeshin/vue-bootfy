e82c180937d803c3f44e5ed7ffcec454a56e9b17
Nick Lim <nicklimvs@gmail.com>
2017-07-13 21:04:21 +0800

    add vForm

---
+++ src/components/vForm/vForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form :class="[formInlineClass]">
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+
+
+export default {
+  props: {
+    inline: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    formInlineClass: function() {
+      if (this.inline) {
+        return 'form-inline';
+      }
+    }
+  }
+};
+</script>
