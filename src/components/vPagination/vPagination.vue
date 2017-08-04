a581b2630bbd00e5b6c1a852dbb3c8049460499e
Nick Lim <nicklimvs@gmail.com>
2017-08-04 12:07:32 +0800

    update pagination

--- src/components/vPagination/vPagination.vue
+++ src/components/vPagination/vPagination.vue
@@ -1,17 +1,19 @@
 <template>
   <nav :aria-label="title">
     <ul :class="['pagination', sizeClass]">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
+      <li class="page-item"
+          :class="[{disabled: currentPage === 1}]">
+        <a class="page-link" href="#" aria-label="Previous" @click.stop.prevent="btnClick(currentPage-1)">
           <span aria-hidden="true">&laquo;</span>
           <span class="sr-only">Previous</span>
         </a>
       </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
+      <li v-for="n in buildRange()" class="page-item" :class="[{ active: currentPage === n}]" >
+        <a class="page-link" @click.stop.prevent="btnClick(n)">{{n}}</a>
+      </li>
+      <li class="page-item"
+          :class="[{disabled: currentPage === totalPages}]">
+        <a class="page-link" href="#" aria-label="Next" @click.stop.prevent="btnClick(currentPage+1)">
           <span aria-hidden="true">&raquo;</span>
           <span class="sr-only">Next</span>
         </a>
@@ -30,6 +32,22 @@ export default {
     size: {
       type: String,
       default: null
+    },
+    totalRecords: {
+      type: Number,
+      default: 0
+    },
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    totalPages: {
+      type: Number,
+      default: 1
+    },
+    max: {
+      type: Number,
+      default: 7
     }
   },
   computed: {
@@ -41,6 +59,29 @@ export default {
           return 'pagination-sm';
         default:
       }
+    }
+  },
+  methods: {
+    buildRange() {
+      var max = this.max;
+
+      if (this.totalPages < max) {
+        max = this.totalPages;
+      }
+
+      const m = Math.floor(max / 2);
+      const last = Math.max(Math.min(this.currentPage + m, this.totalPages), max);
+
+      console.log('Current Page: ' + this.currentPage + ', m ' + m + ', last ' + last);
+      const list = [];
+
+      for (var i = last; i > last - max; i--) {
+        list.push(i);
+      }
+      return list.reverse();
+    },
+    btnClick(page) {
+      this.$emit('click', page);
     }
   }
 };
